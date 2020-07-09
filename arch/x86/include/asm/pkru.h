5250b72fec02a3499c1e8a96f9a473c000ec92aa
Ira Weiny <ira.weiny@intel.com>
2020-07-09 09:17:57 -0700

    x86/pkeys: Create pkeys_common.h
    Protection Keys User (PKU) and Protection Keys Supervisor (PKS) work in
    similar fashions and can share common defines.  Specifically PKS and PKU
    each have:
    
    	1. A single control register
    	2. The same number of keys
    	3. The same number of bits in the register per key
    	4. Access and Write disable in the same bit locations
    
    Given the above, share all the macros that synthesize and manipulate
    register values between the two features.  Share these defines by moving
    them into a new header, change their names to reflect the common use,
    and include the header where needed.
    
    Also while editing the code remove the use of 'we' from comments being
    touched.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- arch/x86/include/asm/pkru.h
+++ arch/x86/include/asm/pkru.h
@@ -3,10 +3,7 @@
 #define _ASM_X86_PKRU_H
 
 #include <asm/fpu/xstate.h>
-
-#define PKRU_AD_BIT 0x1
-#define PKRU_WD_BIT 0x2
-#define PKRU_BITS_PER_PKEY 2
+#include <asm/pkeys_common.h>
 
 #ifdef CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS
 extern u32 init_pkru_value;
@@ -18,18 +15,15 @@ extern u32 init_pkru_value;
 
 static inline bool __pkru_allows_read(u32 pkru, u16 pkey)
 {
-	int pkru_pkey_bits = pkey * PKRU_BITS_PER_PKEY;
-	return !(pkru & (PKRU_AD_BIT << pkru_pkey_bits));
+	int pkru_pkey_bits = pkey * PKR_BITS_PER_PKEY;
+	return !(pkru & (PKR_AD_BIT << pkru_pkey_bits));
 }
 
 static inline bool __pkru_allows_write(u32 pkru, u16 pkey)
 {
-	int pkru_pkey_bits = pkey * PKRU_BITS_PER_PKEY;
-	/*
-	 * Access-disable disables writes too so we need to check
-	 * both bits here.
-	 */
-	return !(pkru & ((PKRU_AD_BIT|PKRU_WD_BIT) << pkru_pkey_bits));
+	int pkru_pkey_bits = pkey * PKR_BITS_PER_PKEY;
+	/* Access-disable disables writes too so check both bits here. */
+	return !(pkru & ((PKR_AD_BIT|PKR_WD_BIT) << pkru_pkey_bits));
 }
 
 static inline u32 read_pkru(void)
