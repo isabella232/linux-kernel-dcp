dffd60ea669c0d4d16f5c15d1db528d55daabf1e
Jacob Pan <jacob.jun.pan@linux.intel.com>
2021-02-04 13:04:32 -0800

    iommu/vt-d: Enable write protect for supervisor SVM
    Write protect bit, when set, inhibits supervisor writes to the read-only
    pages. In supervisor shared virtual addressing (SVA), where page tables
    are shared between CPU and DMA, IOMMU PASID entry WPE bit should match
    CR0.WP bit in the CPU.
    This patch sets WPE bit for supervisor PASIDs if CR0.WP is set.
    
    Signed-off-by: Sanjay Kumar <sanjay.k.kumar@intel.com>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>

--- drivers/iommu/intel/pasid.c
+++ drivers/iommu/intel/pasid.c
@@ -576,7 +576,6 @@ static void pasid_flush_caches(struct intel_iommu *iommu,
 
 static inline int pasid_enable_wpe(struct pasid_entry *pte)
 {
-#ifdef CONFIG_X86
 	unsigned long cr0 = read_cr0();
 
 	/* CR0.WP is normally set but just to be sure */
@@ -584,7 +583,6 @@ static inline int pasid_enable_wpe(struct pasid_entry *pte)
 		pr_err_ratelimited("No CPU write protect!\n");
 		return -EINVAL;
 	}
-#endif
 	pasid_set_wpe(pte);
 
 	return 0;
