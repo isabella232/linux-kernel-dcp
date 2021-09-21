801a385523c19b642728ed8c608d54587a840d32
Tom Zanussi <tom.zanussi@linux.intel.com>
2021-09-21 14:52:44 -0700

    crypto: iax - Introduce iax hardware compression accelerator
    The Intel Analytics Accelerator (IAX) is a hardware accelerator that
    provides very high thoughput compression/decompression combined with
    analytic primitive functions. The primitive functions are commonly
    used for filtering data during analytic query processing. The IAX
    primarily targets applications such as big data and in-memory analytics
    databases as well as application-transparent usages such as memory page
    compression (e.g. zswap compression/decompression).
    
    The IAX supports compression/decompression compatible with the DEFLATE
    compression standard described in RFC 1951, which is the
    compression/decompression algorithm exported by this module.  Users
    can select IAX compress/decompress acceleration by specifying
    'iax_crypto' as the compression algorithm to use by whatever facility
    allows compression algorithms to be selected.
    
    For example, a zram drive can select iax_crypto via:
    
      # echo iax_crypto > /sys/block/zram1/comp_algorithm
    
    Similarly for zswap:
    
      # echo iax_crypto > /sys/module/zswap/parameters/compressor
    
    In order for the iax_crypto module to actually do any
    compression/decompression work on behalf of a facility, one or more
    IAX workqueues need to be bound to the iax_crypto driver.
    
    For instance, here's an example of configuring an IAX workqueue
    and binding it to the iax_crypto driver:
    
      # configure wq1.0
      #
      # echo shared > /sys/bus/dsa/devices/iax1/wq1.0/mode
      # echo "kernel" > /sys/bus/dsa/devices/iax1/wq1.0/type
      # echo "iax_crypto" > /sys/bus/dsa/devices/iax1/wq1.0/name
      # echo 0 > /sys/bus/dsa/devices/iax1/engine1.0/group_id
    
      # enable IAX device iax1
      #
      # echo iax1 > /sys/bus/dsa/drivers/idxd/bind
    
      # enable wq1.0 on IAX device iax1
    
      # echo wq1.0 > /sys/bus/dsa/drivers/crypto/bind
    
    Whenever a new workqueue is bound to or unbound from the iax_crypto
    driver, the available workqueues are 'rebalanced' such that work
    submitted from a particular CPU is given to the most appropriate
    workqueue available.  There currently isn't any way for the user to
    tweak the way this is done internally - if necessary, knobs can be
    added later for that purpose.  Current best practice is to configure
    and bind at least one workqueue for each IAX device, but as long as
    there is at least one workqueue configured and bound to any IAX device
    in the system, the iax_crypto driver will work, albeit most likely not
    as efficiently.
    
    [ Based on work originally by George Powley and Jing Lin. ]
    
    Co-developed-by: Kyung Min Park <kyung.min.park@intel.com>
    Signed-off-by: Kyung Min Park <kyung.min.park@intel.com>
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

---
+++ drivers/crypto/iax/iax_crypto.h
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2021 Intel Corporation. All rights rsvd. */
+
+#ifndef __IAX_CRYPTO_H__
+#define __IAX_CRYPTO_H__
+
+#include <linux/crypto.h>
+#include <linux/idxd.h>
+#include <uapi/linux/idxd.h>
+
+#undef pr_fmt
+#define	pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
+
+#define IAX_DECOMP_ENABLE		BIT(0)
+#define IAX_DECOMP_FLUSH_OUTPUT		BIT(1)
+#define IAX_DECOMP_CHECK_FOR_EOB	BIT(2)
+#define IAX_DECOMP_STOP_ON_EOB		BIT(3)
+#define IAX_DECOMP_SUPPRESS_OUTPUT	BIT(9)
+
+#define IAX_COMP_FLUSH_OUTPUT		BIT(1)
+#define IAX_COMP_APPEND_EOB		BIT(2)
+
+#define IAX_COMPLETION_TIMEOUT		1000000
+
+#define IAX_ANALYTICS_ERROR		0x0a
+#define IAX_ERROR_COMP_BUF_OVERFLOW	0x19
+#define IAX_ERROR_WATCHDOG_EXPIRED	0x24
+
+#define DYNAMIC_HDR			0x2
+#define DYNAMIC_HDR_SIZE		3
+
+#define IAX_COMP_FLAGS			(IAX_COMP_FLUSH_OUTPUT | \
+					 IAX_COMP_APPEND_EOB)
+
+#define IAX_DECOMP_FLAGS		(IAX_DECOMP_ENABLE |	   \
+					 IAX_DECOMP_FLUSH_OUTPUT | \
+					 IAX_DECOMP_CHECK_FOR_EOB | \
+					 IAX_DECOMP_STOP_ON_EOB)
+
+/*
+ * Analytics Engine Configuration and State (AECS) contains parameters and
+ * internal state of the analytics engine.
+ */
+struct aecs_table_record {
+	u32 crc;
+	u32 xor_checksum;
+	u32 reserved0[5];
+	u32 num_output_accum_bits;
+	u8 output_accum[256];
+	u32 ll_sym[286];
+	u32 reserved1;
+	u32 reserved2;
+	u32 d_sym[30];
+	u32 reserved_padding[2];
+};
+#endif
