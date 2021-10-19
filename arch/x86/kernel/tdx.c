intel/tdx

[REVERTME] platform/x86: Add RTMR update interface
Add an experimental interface to update the RTMR measurement registers
in the TDX module from ring 3. Simply allow to pass in a update hash
and pass it on to a TDCALL. The interface has the same rights
as any other operation on the tdx-attest device node, so becomes
controllable by udev rules.

This is currently not for upstream, but mainly for internal
testing.

Signed-off-by: Andi Kleen <ak@linux.intel.com>
Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

arch/x86/kernel/tdx.c
@@ -28,6 +28,7 @@
 
 /* TDX Module call Leaf IDs */
 #define TDINFO				1
+#define TDRMTR_EXTEND			2
 #define TDGETVEINFO			3
 #define TDREPORT			4
 #define TDACCEPTPAGE			6
@@ -232,6 +233,34 @@ int tdx_mcall_tdreport(u64 data, u64 reportdata)
 	return -EIO;
 }
 EXPORT_SYMBOL_GPL(tdx_mcall_tdreport);
+
+/*
+ * tdx_mcall_rtmr_extend() - Extend a TDX measurement register
+ *
+ * @data	: Physical address of 96B aligned data.
+ * @rtmr	: RTMR number
+ *
+ * return 0 on success or failure error number.
+ */
+int tdx_mcall_rtmr_extend(u64 data, u64 rtmr)
+{
+	u64 ret;
+
+	if (!data || !prot_guest_has(PR_GUEST_TDX))
+		return -EINVAL;
+
+	ret = __trace_tdx_module_call(TDRMTR_EXTEND, data, rtmr, 0, 0, NULL);
+
+	if (ret == TDCALL_SUCCESS)
+		return 0;
+	else if (TDCALL_RETURN_CODE(ret) == TDCALL_INVALID_OPERAND)
+		return -EINVAL;
+	else if (TDCALL_RETURN_CODE(ret) == TDCALL_OPERAND_BUSY)
+		return -EBUSY;
+
+	return -EIO;
+}
+EXPORT_SYMBOL_GPL(tdx_mcall_rtmr_extend);
 
 /*
  * tdx_hcall_get_quote() - Generate TDQUOTE using TDREPORT_STRUCT.

isabella232/linux-kernel-dcp

arch/x86/kernel/tdx.c
@@ -27,6 +27,7 @@
 
 /* TDX Module call Leaf IDs */
 #define TDX_GET_INFO			1
+#define TDX_RMTR_EXTEND			2
 #define TDX_GET_VEINFO			3
 #define TDX_GET_REPORT			4
 #define TDX_ACCEPT_PAGE			6
@@ -225,6 +226,34 @@ int tdx_mcall_tdreport(u64 data, u64 reportdata)
 	return -EIO;
 }
 EXPORT_SYMBOL_GPL(tdx_mcall_tdreport);
+
+/*
+ * tdx_mcall_rtmr_extend() - Extend a TDX measurement register
+ *
+ * @data	: Physical address of 96B aligned data.
+ * @rtmr	: RTMR number
+ *
+ * return 0 on success or failure error number.
+ */
+int tdx_mcall_rtmr_extend(u64 data, u64 rtmr)
+{
+	u64 ret;
+
+	if (!data || !cc_platform_has(CC_ATTR_GUEST_TDX))
+		return -EINVAL;
+
+	ret = __trace_tdx_module_call(TDX_RMTR_EXTEND, data, rtmr, 0, 0, NULL);
+
+	if (ret == TDCALL_SUCCESS)
+		return 0;
+	else if (TDCALL_RETURN_CODE(ret) == TDCALL_INVALID_OPERAND)
+		return -EINVAL;
+	else if (TDCALL_RETURN_CODE(ret) == TDCALL_OPERAND_BUSY)
+		return -EBUSY;
+
+	return -EIO;
+}
+EXPORT_SYMBOL_GPL(tdx_mcall_rtmr_extend);
 
 /*
  * tdx_hcall_get_quote() - Generate TDQUOTE using TDREPORT_STRUCT.