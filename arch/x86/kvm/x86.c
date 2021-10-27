d93a968951b14cd31a79f5cc3c65cf2ac9cebd67
Jing Liu <jing2.liu@intel.com>
2021-10-27 05:40:33 -0700

    kvm: x86: AMX XCR0 support for guest
    Two XCR0 bits are defined for AMX to support XSAVE mechanism. Bit 17 is
    for tilecfg and bit 18 is for tiledata.
    
    The value of XCR0[17:18] is always either 00b or 11b. Also, SDM recommends
    that only 64-bit operating systems enable Intel AMX by setting
    XCR0[18:17].
    
    Signed-off-by: Jing Liu <jing2.liu@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -210,7 +210,7 @@ static struct kvm_user_return_msrs __percpu *user_return_msrs;
 #define KVM_SUPPORTED_XCR0     (XFEATURE_MASK_FP | XFEATURE_MASK_SSE \
 				| XFEATURE_MASK_YMM | XFEATURE_MASK_BNDREGS \
 				| XFEATURE_MASK_BNDCSR | XFEATURE_MASK_AVX512 \
-				| XFEATURE_MASK_PKRU)
+				| XFEATURE_MASK_PKRU | XFEATURE_MASK_XTILE)
 
 u64 __read_mostly host_efer;
 EXPORT_SYMBOL_GPL(host_efer);
@@ -1105,6 +1105,16 @@ static int __kvm_set_xcr(struct kvm_vcpu *vcpu, u32 index, u64 xcr)
 			return 1;
 		if ((xcr0 & XFEATURE_MASK_AVX512) != XFEATURE_MASK_AVX512)
 			return 1;
+	}
+
+	if (xcr0 & XFEATURE_MASK_XTILE) {
+#ifdef CONFIG_X86_64
+		if ((xcr0 & XFEATURE_MASK_XTILE) != XFEATURE_MASK_XTILE)
+			return 1;
+#else
+		/* Only 64-bit OS enable AMX by setting XCR0[18:17] */
+		xcr0 &= ~XFEATURE_MASK_XTILE;
+#endif
 	}
 	vcpu->arch.xcr0 = xcr0;
 
