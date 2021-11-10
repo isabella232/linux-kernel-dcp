intel/tdx

x86/cpu/tdx: Introduce boot option to opt-in for the TDX host feature
Introduce an opt-in boot parameter to enable the TDX host feature.

TDX requires the TDX firmware to load and initialize and has a memory
overhead.  The TDX hardware uses 0.39% of system RAM for its metadata the
TDX protects, about 4MB overhead per 1GB. Linux makes all RAM available to
TDX guests.  Because the memory usage is non-negligible, introduce an
opt-in kernel command option.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kernel/cpu/tdx/tdx.c
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Load and initialize TDX-module. */
+
+#define pr_fmt(fmt) "tdx: " fmt
+
+#include <linux/init.h>
+
+#include "p-seamldr.h"
+
+enum TDX_HOST_OPTION {
+	TDX_HOST_OFF,
+	TDX_HOST_ON,
+};
+
+static enum TDX_HOST_OPTION tdx_host __initdata;
+
+static int __init tdx_host_param(char *str)
+{
+	if (!strcmp(str, "on"))
+		tdx_host = TDX_HOST_ON;
+
+	return 0;
+}
+early_param("tdx_host", tdx_host_param);
+
+static int __init tdx_early_init(void)
+{
+	/* Avoid TDX overhead when opt-in is not present. */
+	if (tdx_host != TDX_HOST_ON)
+		return 0;
+
+	/* TODO: Launch NP-SEAMLDR. */
+	return 0;
+}
+early_initcall(tdx_early_init);

isabella232/linux-kernel-dcp

arch/x86/kernel/cpu/tdx/tdx.c
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Load and initialize TDX-module. */
+
+#define pr_fmt(fmt) "tdx: " fmt
+
+#include <linux/init.h>
+
+#include "p-seamldr.h"
+
+enum TDX_HOST_OPTION {
+	TDX_HOST_OFF,
+	TDX_HOST_ON,
+};
+
+static enum TDX_HOST_OPTION tdx_host __initdata;
+
+static int __init tdx_host_param(char *str)
+{
+	if (!strcmp(str, "on"))
+		tdx_host = TDX_HOST_ON;
+
+	return 0;
+}
+early_param("tdx_host", tdx_host_param);
+
+static int __init tdx_host_early_init(void)
+{
+	/* Avoid TDX overhead when opt-in is not present. */
+	if (tdx_host != TDX_HOST_ON)
+		return 0;
+
+	/* TODO: Launch NP-SEAMLDR. */
+	return 0;
+}
+early_initcall(tdx_host_early_init);