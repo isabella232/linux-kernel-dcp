69eaf33f7bd17cb9b2cd4a155b6efc1ad1531b97
Ira Weiny <ira.weiny@intel.com>
2021-12-08 12:20:09 -0800

    x86/pkeys: Enable PKS on cpus which support it
    Protection Keys for Supervisor pages (PKS) enables fast, hardware thread
    specific, manipulation of permission restrictions on supervisor page
    mappings.  It uses a supervisor specific MSR to assign permissions to
    the pkeys.
    
    When PKS is configured and the cpu supports PKS, initialize the MSR, and
    enable the hardware.
    
    Add asm/pks.h to store new internal functions and structures such as
    pks_setup().
    
    Co-developed-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V9
    	Reword commit message
    	Move this after the patch defining PKS_INIT_VALUE
    
    Changes for V8
    	Move setup_pks() into this patch with a default of all access
    		for all pkeys.
    	From Thomas
    		s/setup_pks/pks_setup/
    	Update Change log to better reflect exactly what this patch does.
    
    Conflicts for spr-po:
    	arch/x86/include/uapi/asm/processor-flags.h
    	arch/x86/kernel/cpu/common.c

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -60,6 +60,7 @@
 #include <asm/cpu_device_id.h>
 #include <asm/uv/uv.h>
 #include <asm/sigframe.h>
+#include <asm/pks.h>
 
 #include "cpu.h"
 
@@ -1628,6 +1629,7 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 
 	x86_init_rdrand(c);
 	setup_pku(c);
+	pks_setup();
 	setup_cet(c);
 
 	/*
