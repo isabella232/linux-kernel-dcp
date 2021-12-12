4a8b6afae53ec11e61b4892d8da9cacc7f8e0a19
Chao Gao <chao.gao@intel.com>
2021-12-12 16:29:44 +0800

    x86/cpu/tdx: Prepare to invoke seam_vmx{on,off}_on_each_cpu at runtime
    SEAMCALLs must be invoked from VMX operation. seam_vmx{on,off}_on_each_cpu
    can be reused to enter/exit VMX operation before/after TDX module update.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/kernel/cpu/tdx/seam.h
+++ arch/x86/kernel/cpu/tdx/seam.h
@@ -13,7 +13,7 @@ bool __init seam_get_firmware(struct cpio_data *blob, const char *name);
 int __init seam_init_vmx_early(void);
 void __init seam_init_vmxon_vmcs(struct vmcs *vmcs);
 
-int __init seam_vmxon_on_each_cpu(void);
-int __init seam_vmxoff_on_each_cpu(void);
+int seam_vmxon_on_each_cpu(void);
+int seam_vmxoff_on_each_cpu(void);
 
 #endif /* _X86_TDX_SEAM_H */
