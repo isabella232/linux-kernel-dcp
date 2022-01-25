3c3544512d916534c25472e49fa236c3f874c9a7
Yang Zhong <yang.zhong@intel.com>
2022-01-24 18:06:58 -0800

    Revert "x86/fpu: Prepare KVM for dynamically enabled states"
    This reverts commit 2acc15e8add25ec43c1cb529173a1ce309aace5d.

--- arch/x86/kernel/fpu/core.c
+++ arch/x86/kernel/fpu/core.c
@@ -200,26 +200,6 @@ void fpu_reset_from_exception_fixup(void)
 #if IS_ENABLED(CONFIG_KVM)
 static void __fpstate_reset(struct fpstate *fpstate);
 
-static void fpu_init_guest_permissions(struct fpu_guest *gfpu)
-{
-	struct fpu_state_perm *fpuperm;
-	u64 perm;
-
-	if (!IS_ENABLED(CONFIG_X86_64))
-		return;
-
-	spin_lock_irq(&current->sighand->siglock);
-	fpuperm = &current->group_leader->thread.fpu.guest_perm;
-	perm = fpuperm->__state_perm;
-
-	/* First fpstate allocation locks down permissions. */
-	WRITE_ONCE(fpuperm->__state_perm, perm | FPU_GUEST_PERM_LOCKED);
-
-	spin_unlock_irq(&current->sighand->siglock);
-
-	gfpu->user_perm = perm & ~FPU_GUEST_PERM_LOCKED;
-}
-
 bool fpu_alloc_guest_fpstate(struct fpu_guest *gfpu)
 {
 	struct fpstate *fpstate;
@@ -235,11 +215,7 @@ bool fpu_alloc_guest_fpstate(struct fpu_guest *gfpu)
 	fpstate->is_valloc	= true;
 	fpstate->is_guest	= true;
 
-	gfpu->fpstate		= fpstate;
-	gfpu->user_xfeatures	= fpu_user_cfg.default_features;
-	gfpu->user_perm		= fpu_user_cfg.default_features;
-	fpu_init_guest_permissions(gfpu);
-
+	gfpu->fpstate = fpstate;
 	return true;
 }
 EXPORT_SYMBOL_GPL(fpu_alloc_guest_fpstate);
