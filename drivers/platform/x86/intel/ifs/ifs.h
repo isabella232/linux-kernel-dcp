03eb96c417b403f5ea737b6a5cfdc159cb22190c
Jithu Joseph <jithu.joseph@intel.com>
2022-01-31 16:45:33 -0800

    platform/x86/intel/ifs: Authenticate and copy to secured memory
    The IFS image contains hashes that will be used to authenticate the ifs
    test chunks. First, use WRMSR to copy the hashes and enumerate the number
    of test chunks, chunk size and the maximum number of cores that can run
    scan test simultaneously.
    
    Next, use WRMSR to authenticate each and every scan test chunk which is
    also stored in the IFS image. The CPU will check if the test chunks match
    the hashes, otherwise failure is indicated to system software. If the test
    chunk is authenticated, it is automatically copied to secured memory.
    
    The ifs hash copy and authentication only needs to be done on the first
    logical cpu of each socket.
    
    Originally-by: Kyung Min Park <kyung.min.park@intel.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>

--- drivers/platform/x86/intel/ifs/ifs.h
+++ drivers/platform/x86/intel/ifs/ifs.h
@@ -14,12 +14,45 @@
 #define MSR_IA32_CORE_CAPS_INTEGRITY_BIT	2
 #define MSR_IA32_CORE_CAPS_INTEGRITY		BIT(MSR_IA32_CORE_CAPS_INTEGRITY_BIT)
 
+#define MSR_COPY_SCAN_HASHES			0x000002c2
+#define MSR_SCAN_HASHES_STATUS			0x000002c3
+#define MSR_AUTHENTICATE_AND_COPY_CHUNK		0x000002c4
+#define MSR_CHUNKS_AUTHENTICATION_STATUS	0x000002c5
+
+/* MSR_SCAN_HASHES_STATUS bit fields */
+union ifs_scan_hashes_status {
+	u64	data;
+	struct {
+		u64	chunk_size	:16;
+		u64	num_chunks	:8;
+		u64	rsvd1		:8;
+		u64	error_code	:8;
+		u64	rsvd2		:11;
+		u64	max_core_limit	:12;
+		u64	valid		:1;
+	};
+};
+
+/* MSR_CHUNKS_AUTH_STATUS bit fields */
+union ifs_chunks_auth_status {
+	u64	data;
+	struct {
+		u64	valid_chunks	:8;
+		u64	total_chunks	:8;
+		u64	rsvd1		:16;
+		u64	error_code	:8;
+		u64	rsvd2		:24;
+	};
+};
+
 /**
  * struct ifs_params - global ifs parameter for all cpus.
  * @loaded_version: stores the currently loaded ifs image version.
+ * @valid_chunks: number of chunks which could be validated.
  */
 struct ifs_params {
 	int loaded_version;
+	int valid_chunks;
 };
 
 int load_ifs_binary(void);
